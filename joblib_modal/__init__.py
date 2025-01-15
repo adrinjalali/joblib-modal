6c49345a0e54ca7458e18701d8b35a1ae013cf96
adrinjalali <adrin.jalali@gmail.com>
2025-01-15 15:02:20 +0100

    increase version

--- joblib_modal/__init__.py
+++ joblib_modal/__init__.py
@@ -3,6 +3,6 @@ from joblib_modal._modal import ModalBackend
 
 register_parallel_backend("modal", ModalBackend)
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = ["ModalBackend", "__version__"]
